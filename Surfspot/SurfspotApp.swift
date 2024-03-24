bfd0d3ad7e000ace8d8bf4eb722424b2fcc856de
amird1WasTaken <amir.dayagi@gmail.com>
2024-03-24 16:53:30 -0700

    Initial Commit

---
+++ Surfspot/SurfspotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurfspotApp.swift
+//  Surfspot
+//
+//  Created by Amir Dayagi on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SurfspotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
